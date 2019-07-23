f00d8dd237b1db1e90bc9601ac0b0c1e74ad813c
ki_blank <lbin7310@gmail.com>
2019-07-23 11:37:48 +0900

    Set cors and route

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const routes = require('./routes');
-const userRouter = require('./routers/userRouter')
+const router= require('./routers');
+const cors = require('cors');
 
 const app = express();
 
@@ -14,6 +15,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use(routes.users, userRouter);
+app.use(cors());
+
+app.use("/", router);
+app.use(routes.username, router);
+
+// board
+
+app.use(routes.userIdBoard, router);
+app.use(routes.boards + routes.create, router);
+
+// container
+
+app.use(routes.containerBoardId, router);
+app.use(routes.containers + routes.create, router);
+
+// card
+
+app.use(routes.cards, router);
+app.use(routes.cards + routes.create, router);
+
+// description
+
+app.use(routes.descriptionCardId, router);
+app.use(routes.description + routes.create, router);
 
 module.exports = app;
