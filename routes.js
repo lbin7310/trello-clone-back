e959093ba92ebd5a1dc62de65e3edafacd61200b
ki_blank <lbin7310@gmail.com>
2019-07-23 11:40:55 +0900

    Create routes and Add routes' ' ;

--- routes.js
+++ routes.js
@@ -1,12 +1,28 @@
-// Users
 
 const USERS = "/";
-
+const USERNAME = "/user/:username";
+const BOARD_USERID = "/board/:board_userId";
+const CONTAINER_BOARDID = "/container/:container_boardId";
+const CARDS = "/cards";
+const DESCRIPTION_CARDID = "/description/:cardId";
+const BOARDS = "/boards";
+const CREATE = "/create";
+const CONTAINERS = "/containers";
+const DESCRIPTION = "/description";
 
 // routes
 
 const routes = {
-  users: USERS
+  users: USERS,
+  username: USERNAME,
+  userIdBoard: BOARD_USERID,
+  containerBoardId: CONTAINER_BOARDID,
+  cards: CARDS,
+  descriptionCardId: DESCRIPTION_CARDID,
+  boards: BOARDS,
+  create: CREATE,
+  containers: CONTAINERS,
+  description: DESCRIPTION
 }
 
 module.exports = routes;
